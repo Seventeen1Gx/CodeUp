b99cb623e9b3115a643ac68e6223efb3390afa8e
GaoXiang <550734027@qq.com>
2018-05-01 14:26:14 +0800

    2018/05/01/Aft Amend

--- 未分类题目Ⅰ-入门题/1058 寻找独一无二的数[位运算].cpp
+++ 未分类题目Ⅰ-入门题/1058 寻找独一无二的数[位运算].cpp
@@ -1,4 +1,10 @@
 #include <stdio.h>
+//异或操作，同零异一
+//有交换律即 a^b = b^a
+//有结合律即 (a^b)^c = a^(b^c)
+//且 a^a = 0，a^0 = a
+//除了独一无二的数其余的数都出现了偶数次 
+//结果 = n1 ^ n2 ^ n3 ^ ……^nn
 
 int main()
 {
@@ -6,7 +12,7 @@ int main()
 	scanf("%d", &n);
 	
 	while (n) {
-		special = 0;
+		special = 0;					//初始化为0 
 		for (int i=n; i>0; i--) {
 			scanf("%d", &a);
 			special ^= a;
